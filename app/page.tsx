bf60853d2b1a0f78d4da63912c1358feb2fbdaa7
priyanshuisrani02-commits <priyanshuisrani02@gmail.com>
2026-08-14 15:04:31 +0530

    Use corrected New Arrivals component

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,18 @@ import Navbar from "@/components/Navbar";
 import HeroCarousel from "@/components/HeroCarousel";
 import CollectionGrid from "@/components/CollectionGrid";
 import FeaturedBanner from "@/components/FeaturedBanner";
-import NewArrivals from "@/components/NewArrivals";
+import NewArrivals from "@/components/NewArrivalsFixed";
 import Footer from "@/components/Footer";
-
 
 export default function Home() {
   return (
     <main className="bg-white text-black">
- <Navbar />
-<HeroCarousel />
-<CollectionGrid />
-<FeaturedBanner />
-<NewArrivals />
-<Footer />
+      <Navbar />
+      <HeroCarousel />
+      <CollectionGrid />
+      <FeaturedBanner />
+      <NewArrivals />
+      <Footer />
     </main>
- 
-);
+  );
 }
